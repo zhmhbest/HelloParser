6565eb5edb870f94333cca43eb319c81fee66559
zhmhbest <zhmhbest@gmail.com>
2020-12-22 17:25:51 +0800

    update

--- src/calc_ast/index.c
+++
@@ -1,7 +0,0 @@
-#include "flex_bison.h"
-
-int main()
-{
-    printf("> ");
-    return yyparse();
-}
--- src/calc_logic/index.c
+++
@@ -1,9 +0,0 @@
-#include "flex_bison.h"
-#include "ast.h"
-
-int main()
-{
-  printf("> ");
-  return yyparse();
-}
-
